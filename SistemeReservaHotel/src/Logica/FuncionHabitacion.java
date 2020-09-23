7856716be563cf4e330ec1e2e973d6327df506bf
iodenKisuka <iodenteken@hotmail.com>
2020-09-22 22:19:49 -0300

    falta terminar

--- SistemeReservaHotel/src/Logica/FuncionHabitacion.java
+++ SistemeReservaHotel/src/Logica/FuncionHabitacion.java
@@ -1,9 +1,12 @@
 
 package Logica;
 
+import Datos.Vhabitacion;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class FuncionHabitacion {
@@ -18,26 +21,89 @@ public class FuncionHabitacion {
         DefaultTableModel modelo;
         // un arreglo con los nombre de las columnas
         String [] titulos={"ID ", "Número","Piso","Descripciónn", "Caracteristicas","Precio diario","Estado","Tipo de habitacion"};
+        String[] registros= new String[8];
         totalRegistro = 0;
         modelo = new DefaultTableModel(null,titulos);
         
         sSQL = "Select * from habitacion where piso like '%"+ buscar+"%' order by id_habitacion";
         
-        //Falta Try Catch y return
         try {
             Statement stateM = conec.createStatement();
             ResultSet resultS = stateM.executeQuery(sSQL);
             
             while(resultS.next()){
-                registro[0]= resultS.getString("idhabitacion");
+                registros[0]= resultS.getString("idhabitacion");
+                registros[1]= resultS.getString("numero");
+                registros[2]= resultS.getString("piso");
+                registros[3]= resultS.getString("descripcion");
+                registros[4]= resultS.getString("caracteristicas");
+                registros[5]= resultS.getString("precio_diario");
+                registros[6]= resultS.getString("estado");
+                registros[7]= resultS.getString("tipo_habitacion");
+                
+                totalRegistro++;
+                modelo.addRow(registros);
+                return modelo;
             }
             
         } catch (Exception e) {
-            
-            
+            JOptionPane.showConfirmDialog(null,e);
+            return null;
         }
+        return null;
         
     }
+    
+    public boolean insertar (Vhabitacion datos){
+        sSQL="insert into habitacion (numero,piso,descripcion,caracteristicas,precio_diario,estado, tipo_habitacion)"+
+                "value (?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement prepareSt = conec.prepareStatement(sSQL) ;
+            prepareSt.setString(1,datos.getNumero());
+            prepareSt.setString(2,datos.getPiso());
+            prepareSt.setString(3,datos.getDescripcion());
+            prepareSt.setString(4,datos.getCaracteristicas());
+            prepareSt.setDouble(5,datos.getPrecio_diario());
+            prepareSt.setString(6,datos.getEstado());
+            prepareSt.setString(7,datos.getTipo_habitacion());
+            
+            int num = prepareSt.executeUpdate();
+            if(num!=0){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+            
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null,e);
+            return false;
+        }
+       
+    }
+     public boolean editar(Vhabitacion datos){
+        sSQL="update habitacion set numero=?, piso=?,descripcion=?,caracteristicas=?,precio_diario=?,estado=?, tipo_habitacion=?";
+        try {
+            
+            
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null,e);
+        }
+       
+    }
+     
+      public boolean eliminar (Vhabitacion datos){
+        
+        try {
+            
+            
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null,e);
+        }
+       
+    }
+ 
  
     
 }
